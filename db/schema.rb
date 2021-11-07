32c4f5922813314ad578fbcd5fa950750f9a07b8
climb719 <mary.oldweiler@gmail.com>
2021-11-07 15:26:06 -0500

    generate user resource and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_03_135834) do
+ActiveRecord::Schema.define(version: 2021_11_07_202426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "palettes", force: :cascade do |t|
     t.text "colors", default: [], array: true
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
