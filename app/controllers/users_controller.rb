b9b738b78be69a5e6a16b4268593352e3c8e22c1
climb719 <mary.oldweiler@gmail.com>
2021-11-08 11:36:59 -0500

    update users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
 
 
+    def index
+        render json: User.all
+    end
+
+
+
     def create
-        user = User.create(params.permit(:username, :password, :password_confirmation))
+        user = User.create(user_params)
+        render json: user
+    end
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :password_confirmation, :biography, :public, :photo)
     end
 end
