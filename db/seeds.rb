248d3b60351b1537810f57c5b24185e9d0d78240
climb719 <mary.oldweiler@gmail.com>
2021-11-08 10:14:14 -0500

    update seed file reset db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,14 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 palettes = Palette.create([
-    {colors: ['#F75F1C', '#FF9A00 ', '#000000', '#881EE4', '#85E21F' ]},
-    {colors: ['#000000', '#ff6a1f', '#ff861f', '#ffffff' ]},
-    {colors: ['#95B645', ' #FEB937 ', '#EFE1C6', '#FAF6EA', '#66666E' ]},
-    {colors: ['#fecf6d', '#99e9eb ', '#2d884d', '#4091d7', '#b34045' ]},
+    {colors: ['#89B5AF', '#96C7C1', '#DED9C4', '#D0CAB2' ]},
+    {colors: ['#193498', '#113CFC', '#1597E5', '#69DADB' ]},
     {colors: ['#a2b9bc', '#b2ad7f', '#878f99', '#6b5b95' ]},
     {colors: ['#6b5b95', '#feb236', '#d64161', '#ff7b25' ]},
+    {colors: ['#FCECDD', '#FFC288', '#FEA82F', '#FF6701' ]},
+    {colors: ['#000000',  '#3D0000', '#950101','#FF0000' ]},
+    {colors: ['#420516',  '#7D1935', '#B42B51','#E63E6D' ]},
+    {colors: ['#321F28',  '#734046', '#A05344','#E79E4F' ]},
     {colors: ['#d5e1df', '#e3eaa7', '#b5e7a0', '#86af49' ]},
     {colors: ['#b9936c', '#dac292', '#e6e2d3', '#c4b7a6' ]},
     {colors: ['#d5f4e6', '#80ced6', '#fefbd8', '#618685' ]},
@@ -23,7 +25,12 @@ palettes = Palette.create([
     {colors: ['#cfe0e8', '#b7d7e8', '#87bdd8', '#daebe8' ]},
     {colors: ['#fbefcc', '#f9ccac', '#f4a688', '#e0876a' ]},
     {colors: ['#fff2df', '#d9ad7c', '#a2836e', '#674d3c' ]},
-    {colors: ['#f9d5e5', '#eeac99', '#e06377', '#c83349' ]}
+    {colors: ['#f9d5e5', '#eeac99', '#e06377', '#c83349' ]},
+    {colors: ['#F6D7A7', '#F6EABE', '#C8E3D4', '#87AAAA' ]},
+    {colors: ['#F75F1C', '#FF9A00 ', '#000000', '#881EE4', '#85E21F' ]},
+    {colors: ['#000000', '#ff6a1f', '#ff861f', '#ffffff' ]},
+    {colors: ['#95B645', ' #FEB937 ', '#EFE1C6', '#FAF6EA', '#66666E' ]},
+    {colors: ['#fecf6d', '#99e9eb ', '#2d884d', '#4091d7', '#b34045' ]},
   ]
 
 )
