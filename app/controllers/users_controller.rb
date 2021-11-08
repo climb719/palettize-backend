c9c5d7222e95609bc0b74db9bb6d6c9d47298d55
climb719 <mary.oldweiler@gmail.com>
2021-11-08 12:38:12 -0500

    add uniquess true for username and if else in create for errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,12 @@ class UsersController < ApplicationController
 
 
     def create
-        user = User.create(user_params)
-        render json: user
+        user = User.new(user_params)
+        if user.save
+            render json: user
+        else
+            render json: {errors: user.errors.full_messages}
+        end
     end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-    
+    validates :username, uniqueness: true
     has_secure_password
 end
