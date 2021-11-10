a3adbb54aaba8fd95a65305f82bdf128e37d923d
climb719 <mary.oldweiler@gmail.com>
2021-11-10 13:57:26 -0500

    update routes, add nested route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  resources :saves
+  resources :saves, only: [:delete]
   resources :sessions, only: [:create]
-  resources :users
-  resources :palettes
+  resources :users, only: [:index, :create]
+ 
+
+  resources :palettes do
+    resources :save, only: [:create]  # for POST 
+ end 
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
