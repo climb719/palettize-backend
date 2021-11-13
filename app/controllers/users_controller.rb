8d7b87264dcb7bfec61da8a6722fa6bab23275b7
climb719 <mary.oldweiler@gmail.com>
2021-11-13 09:52:12 -0500

    update session and user create to take in token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     def create
         user = User.new(user_params)
         if user.save
-            render json: user
+            render json: {user: UserSerializer.new(user), token: encode_token(user.id)}
         else
             {errors: user.errors.full_messages}
         end
