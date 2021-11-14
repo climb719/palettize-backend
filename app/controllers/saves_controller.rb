97e682c0ceddc9d49a9d26ec191d2621cc22de9a
climb719 <mary.oldweiler@gmail.com>
2021-11-14 12:46:03 -0500

    finding palette from create based on params, testing render with embded token

--- app/controllers/saves_controller.rb
+++ app/controllers/saves_controller.rb
@@ -4,8 +4,18 @@ class SavesController < ApplicationController
 
 
     def create
-        user = logged_in_user
-        byebug
+       #byebug
+        palette = Palette.find_by(id: params[:palette_id])
+      
+        render json: {palette: PaletteSerializer.new(palette), token: encode_token(logged_in_user.id)}
+ 
+        # else
+        #   render json: palette.errors, status: :unprocessable_entity
+        # end
+       
     end
-    
+
+
+   
+
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username
-
-  //has_many :palettes, each_serializer: PaletteSerializer
  
 end
