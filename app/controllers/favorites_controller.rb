206b03fbfc60b58e88a2e5d0577e4c033d4390da
climb719 <mary.oldweiler@gmail.com>
2021-11-15 09:56:33 -0500

    troubleshooting create in favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,7 +2,24 @@ class FavoritesController < ApplicationController
 
 
     def create
-        palette = Palette.find_by(id: params[:palette_id])
+        set_palette
+        palette = logged_in_user.palettes.build(id: @palette.id, colors: @palette.colors, tags: @palette.tags)
         byebug
+       
+     end
+ 
+ 
+    private
+ 
+    def set_palette
+       @palette = Palette.find_by(id: params[:palette_id])
     end
 end
+
+
+
+#     def create
+#         palette = Palette.find_by(id: params[:palette_id])
+#         logged_in_user.palettes.build(id: palette.id, colors: palette.colors, tags: palette.tags)    
+#      end
+# end
