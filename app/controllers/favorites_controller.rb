9f57fb66e82b59b1324c4aeea743633e45313ebb
climb719 <mary.oldweiler@gmail.com>
2021-11-16 18:27:55 -0500

    createing favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,25 +6,20 @@ class FavoritesController < ApplicationController
     end
 
     def create
-     set_palette_and_user_id
-        byebug
-     favorite = Favorite.create(favorite_params)
-     if favorite.save
-        byebug
-     end
+        palette_id = Palette.find_by(id: params[:palette_id]).id   
+        favorite = logged_in_user.favorites.build(palette_id: palette_id)
+        if favorite.save
+            render json: logged_in_user.favorites
+        else
+            render json: favorite.errors, status: :unprocessable_entity
+        end
     end
  
  
-    private
- 
-    def set_palette_and_user_id
-       @palette = Palette.find_by(id: params[:palette_id]).id
-       @user = logged_in_user.id
-    end
 
-    def favorite_params
-        params.require(:favorite).permit(:user_id, :palette_id)
-    end
+    # def favorite_params
+    #     params.require(:favorite).permit(:user_id, :palette_id)
+    # end
 
     
     
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
 
-  resources :sessions
-  resources :users, only: [:index, :create]
+  resources :sessions, only: [:create]
+
+  resources :users, only: [:index, :create, :show] 
+  
+  # do
+  #   resources :favorites, only: [:show]
+  # end
 
   resources :palettes do
-    resources :favorites
+    resources :favorites, only: [:create]
  end 
 
  get "/autologin", to: 'sessions#autologin'
